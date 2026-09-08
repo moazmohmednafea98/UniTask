33be5d1bc5f5a58e686014cc2a3ce8920bde455e
moazmohmednafea98 <moazmohmednafea98@gmail.com>
2026-09-09 01:36:46 +0300

    Add configuration files (TypeScript, Vite, Tailwind, PostCSS, HTML, main entry)

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: '#f0f7ff',
+          100: '#e0effe',
+          200: '#c7e0fd',
+          300: '#a3cbfc',
+          400: '#7bb3f8',
+          500: '#5a95f5',
+          600: '#4170f0',
+          700: '#2e4be3',
+          800: '#2940c9',
+          900: '#2b35a3',
+        },
+      },
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
+  darkMode: 'class',
+  plugins: [],
+}
